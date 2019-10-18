a83c056a6d4fd2eae9f166a5db1dfb2a120a65c9
patrik <patrikvuhuu@gmail.com>
2019-10-18 17:33:01 +0200

    a

---
+++ MiniProject/Part1.fs
@@ -0,0 +1,35 @@
+﻿module Part1
+
+open System.Text
+open System.Text
+
+type Form =
+    | Bottle
+    | Cup
+    | Can;;
+
+type Size =
+    | Large
+    | Medium
+    | Small;;
+
+type DrinkType =
+    | Latte
+    | Cappuccino
+    | Cortado
+    | Monster
+    | Redbull
+    | Tuborg
+    | Smoothie
+    | Drikkeyoghurt
+    | Sodavand;;
+
+type Drink = {Name: DrinkType; Form: Form; Size: Size}
+
+let price (drink: Drink) =
+    match drink.Size with
+     | Large -> 20
+     | Medium -> 15
+     | Small -> 10;;
+
+let x = {Name= Latte; Form = Cup; Size = Medium};;
